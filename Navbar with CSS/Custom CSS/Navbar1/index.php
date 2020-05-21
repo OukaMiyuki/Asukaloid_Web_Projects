b2e8b2465256c13557e0ebe61d1e58b8eb6aae3d
OukaMiyuki <asukaloid.blog@gmail.com>
2020-05-22 05:39:19 +0700

    Commit Awal

---
+++ Navbar with CSS/Custom CSS/Navbar1/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Navbar 1</title>
+	<meta name="description" content="content">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<img class="logo" src="images/logo.svg" alt="just a logo">
+		<nav>
+			<ul class="nav_links">
+				<li><a href="#" title="">Services</a></li>
+				<li><a href="#" title="">Projects</a></li>
+				<li><a href="#" title="">About</a></li>
+			</ul>
+		</nav>
+		<a href="#" class="cta"><button type="button">Contact</button></a>
+	</header>
+</body>
+</html>
