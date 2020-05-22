4e7cbc673a0daf5f9c95321ab4675c0245d973e7
OukaMiyuki <asukaloid.blog@gmail.com>
2020-05-22 22:10:24 +0700

    tambahan bootsrap navbar

---
+++ Navbar with CSS/Bootstrap_Navbar/Navbar1 (With Responsive Banner)/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="content">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Navbar 1 & Resopnsive Banner</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light navbar-inner bg-light">
+			<div class="container-fluid">
+				<a class="navbar-brand" href="">Navbar</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMenu" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navMenu">
+					<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Home <span class="sr-only">(Current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="">All Manga</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="">Genre</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="">Reviews</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="">About</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="">Github</a>
+						</li>
+					</ul>
+					<form class="form-inline my-2 my-lg-0">
+						<input class="form-control mr-sm-2" type="search" placeholder="Search"></input>
+						<button type="submit" class="btn btn-outline-success my-2 my-sm-0">Search</button>
+					</form>
+				</div>
+			</div>
+		</nav>
+	</header><!-- /header -->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
